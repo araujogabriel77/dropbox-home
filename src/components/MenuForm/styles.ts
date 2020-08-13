48fa733891bcd1adc9feae29d65aa60b80cab1db
araujogabriel77 <araujogabrielocn@gmail.com>
2020-08-12 23:54:40 -0300

    att

---
+++ src/components/MenuForm/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { FaDropbox } from 'react-icons/fa';
+
+export const Container = styled.div`
+  
+`;
+export const Navigation = styled.div`
+
+`;
+export const DropboxLogo = styled(FaDropbox)`
+
+`;
+export const Form = styled.div`
+
+`;
